9e1ce5e3227d67b0b824eb34ee1a199cf7c50933
jamadam <sugama@jamadam.com>
2012-09-01 16:11:27 +0900

    made official site portable

--- official-site.pl
+++ official-site.pl
@@ -10,6 +10,8 @@ use Marquee;
 
 $ENV{MOJO_HOME} = File::Spec->rel2abs(dirname(__FILE__));
 
+my $officical_domain = 'http://marquee.jamadam.com';
+
 my $app = Marquee->new;
 
 #$app->under_development(1);
@@ -20,7 +22,17 @@ my $md  = $app->plugin(Markdown => {no_route => 1});
 $app->plugin(Router => sub {
     my $r = shift;
     $r->route(qr{^/(.+\.md)?$})->to(sub {
-        $md->serve_markdown($app->search_static(shift || 'readme.md'))
+        $md->serve_markdown($app->search_static(shift || 'readme.md'));
+        
+        my $res = Marquee->c->tx->res;
+        my $dom = $res->dom;
+        $dom->find("a[href^=$officical_domain/]")->each(sub {
+            my $e = shift;
+            my $org = $e->attrs('href');
+            $org =~ s{^\Q$officical_domain\E}{};
+            $e->attrs('href', $org);
+        });
+        $res->body($dom);
     });
     $r->route(qr{^/perldoc/(.+)})->to(sub {
         $pod->serve_pod_by_name(shift);
